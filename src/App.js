7f940f32f04f8cdab0c22e618e4a9c451605aef6
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2022-02-20 13:09:03 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleFirstNameChange = (e) => setFirstName(e.target.value);
+
+  const handleLastNameChange = (e) => setLastName(e.target.value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='header'>
+        <Header />
+      </div>
+      <div className='content'>
+        <input type="text" onChange={handleFirstNameChange} placeholder='Please key first name' />
+        <br />
+        <input type="text" onChange={handleLastNameChange} placeholder='Please key last name' />
+        <br />
+        firstName: {firstName}
+        <br />
+        lastName: {lastName}
+        <hr />
+      </div>
+      <div className='footer'></div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <>
+            <div className='menu'>
+                <a href="/">Home</a>
+            </div>
+            <div className='menu'>
+                <a href="/">Product</a>
+            </div>
+            <div>
+                <div className='menu'>
+                    <a href="/">Service</a>
+                </div>
+            </div>
+            <div className='menu'>
+                <a href="/">About</a>
+            </div>
+        </>
+    );
+}
+
+export default Header;
+
+// import React from 'react';
+
+// function Header(props) {
+//     console.log('header props', props.name);
+//     return (
+//         <>
+//             <div className='menu'>
+//                 <a href='/'>Home</a>
+//             </div>
+//             <div className='menu'>
+//                 <a href='/'>Product</a>
+//             </div>
+//             <div className='menu'>
+//                 <a href='/'>Service</a>
+//             </div>
+//             <div className='menu'>
+//                 <a href='/'>About</a>
+//             </div>
+//             <div>Hello , {props.name}</div>
+//         </>
+//     );
+// }
+
+// export default Header;
